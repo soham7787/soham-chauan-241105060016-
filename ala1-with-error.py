8223c82412526c70b21c9891f7e09f590be4a404
soham7787 <nipamnipam41437@gmail.com>
2026-02-23 09:47:50 +0530

    Add files via upload

---
+++ ala1-with-error.py
@@ -0,0 +1,19 @@
+print("Railway Reservation")
+trains = ["Express", "Local", "Superfast"]
+seats = [50, 30, 20]
+name = input("Enter passenger name: ")
+train = input("Enter train name: ")
+tickets = input("Enter number of tickets: ")
+index = trains.index(train)
+if seats[index] >= tickets
+seats[index] = seats[index] - tickets
+else
+print("Not enough seats")
+fare = 300
+amount = tickets * fare
+print("Passenger:", name)
+print("Train:", train)
+print("Amount:", Amount)
+for i in range(2)
+print("Booked")
+print("End")
